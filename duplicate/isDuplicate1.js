8a8424bd7c5ec5de873016485e6408e47d2e016d
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-11-29 17:28:45 -0800

    comment added

--- duplicate/isDuplicate1.js
+++ duplicate/isDuplicate1.js
@@ -1,6 +1,6 @@
 const isDuplicate1 = function(arr){
 
-
+// we can check first index and last index of an element, if they are all identical, this is goona be true;
   for(let i=0; i<arr.length; i++){
      if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])){
        return true;
