5deb50ac43204049d09f5970ee7d1bf7fa73e5a3
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-11-29 17:34:53 -0800

    this is the second duplicate method

---
+++ duplicate/isDuplicate2.js
@@ -0,0 +1,16 @@
+const isDuplicate2 = function(arr){
+  // we can sort the arr to see if ajacent elements are identical;
+
+  arr = arr.sort((a,b)=>a-b);
+
+   for(let i =0; i<arr.length; i++){
+     if(arr[i] === arr[i+1]){
+       return true;
+     }
+
+   }
+   return false;
+  
+  }
+
+  console.log(isDuplicate2([1,2,3,4,5]))
