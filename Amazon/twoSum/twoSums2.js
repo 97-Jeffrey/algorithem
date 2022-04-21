e491cb53e6aaec6ecb8d0082d17f47d1293f6c94
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2022-04-20 23:23:01 -0700

    ...

--- Amazon/twoSum/twoSums2.js
+++ Amazon/twoSum/twoSums2.js
@@ -6,12 +6,12 @@ const twoSums  = (nums, target) =>{
   
   let storage = {};
   for(let i=0; i<nums.length; i++){
-    if(storage[nums[i]]){
+    if(storage[nums[i]]!==undefined){
        return [storage[nums[i]], i]
-    }else{
-        storage[target-nums[i]] = i
     }
+    storage[target-nums[i]] = i
+    console.log(storage)
   }
 }
 
-console.log(twoSums([2,2,5,3,6], 7))
+console.log(twoSums([2,7,11,15],9))
